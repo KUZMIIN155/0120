9871c73bf85f31bd7e1797c75b602e1ddc34bed4
Kirill Ruslan <kirill.smolen@yandex.ru>
2022-01-20 12:09:15 +0700

    start

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include <QCoreApplication>
+#include "student.h"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    Student s1;
+    s1.print();
 
-    return a.exec();
+    return 0;
 }
